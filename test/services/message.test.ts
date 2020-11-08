98498155ba369275c2426d7101fdbfcdb2013fb3
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-11-08 22:01:26 +0700

    Remove message from test

--- test/services/message.test.ts
+++
@@ -1,8 +0,0 @@
-import app from "../../src/app";
-
-describe("'messages' service", () => {
-  it("registered the service", () => {
-    const service = app.service("messages");
-    expect(service).toBeTruthy();
-  });
-});
