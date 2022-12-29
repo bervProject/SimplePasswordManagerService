cf4e32cd31acd169b928a8259e55a538ac2f4a4d
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-12-29 17:25:49 +0700

    Fix code build

--- SimplePasswordManagerService/Repositories/CredentialRepo.cs
+++ SimplePasswordManagerService/Repositories/CredentialRepo.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.CodeAnalysis.Options;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 using SimplePasswordManagerService.Models;
 using SimplePasswordManagerService.Utils;
