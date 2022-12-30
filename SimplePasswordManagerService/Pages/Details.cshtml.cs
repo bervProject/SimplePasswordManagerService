87a9959cf9cf469fadac42a51d514b0558f59572
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-12-30 15:59:31 +0700

    Init Testings

--- SimplePasswordManagerService/Pages/Details.cshtml.cs
+++ SimplePasswordManagerService/Pages/Details.cshtml.cs
@@ -1,9 +1,12 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SimplePasswordManagerService.Models;
 using SimplePasswordManagerService.Repositories;
 
 namespace SimplePasswordManagerService.Pages;
 
+[Authorize]
 public class Details : PageModel
 {
   private readonly ICredentialRepo _credentialRepo;
@@ -16,6 +19,11 @@ public class Details : PageModel
 
   public void OnGet(Guid id)
   {
-    Data = _credentialRepo.GetCredentialsById(id, true);
+    var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+    if (string.IsNullOrWhiteSpace(userId))
+    {
+      return;
+    }
+    Data = _credentialRepo.GetCredentialsById(id, userId, true);
   }
 }
