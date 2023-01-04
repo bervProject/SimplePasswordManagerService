271319a32682d11f6f4e48f769469bc6c249a16d
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-01-04 14:00:41 +0700

    Add redirect uri

--- SimplePasswordManagerService/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ SimplePasswordManagerService/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -6,10 +6,15 @@ namespace SimplePasswordManagerService.Areas.Identity.Pages.Account;
 
 public class Login : PageModel
 {
+  private readonly string? _redirectUri;
+  public Login(IConfiguration configuration)
+  {
+    _redirectUri = configuration["Authentication:Microsoft:RedirectUri"];
+  }
   public IActionResult OnGet()
   {
     var props = new AuthenticationProperties();
-    props.RedirectUri = "/";
+    props.RedirectUri = _redirectUri;
     return Challenge(props);
   }
 }
