91eaded97c1d8488ebb2729d63c38e4bad3308e7
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-03-08 20:24:28 +0700

    Update Program.cs

--- SimplePasswordManagerService/Program.cs
+++ SimplePasswordManagerService/Program.cs
@@ -14,46 +14,43 @@ var mongoConnectionString = builder.Configuration.GetConnectionString("mongo");
 // Add secret settings
 builder.Services.Configure<EncryptSettings>(builder.Configuration.GetSection("EncryptSettings"));
 
-if (!string.IsNullOrWhiteSpace(mongoConnectionString))
-{
+if (!string.IsNullOrWhiteSpace(mongoConnectionString)) {
   var mongoClient = new MongoClient(mongoConnectionString);
   builder.Services.AddSingleton<IMongoClient>(mongoClient);
 }
 builder.Services.AddScoped<ICredentialRepo, CredentialRepo>();
 // add authentication
 builder.Services.AddAuthentication(
-  options =>
-  {
+  options => {
     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = MicrosoftAccountDefaults.AuthenticationScheme;
   })
   .AddCookie()
-  .AddMicrosoftAccount(microsoftOptions =>
-{
-  var clientId = builder.Configuration["Authentication:Microsoft:ClientId"];
-  var clientSecret = builder.Configuration["Authentication:Microsoft:ClientSecret"];
-  if (string.IsNullOrWhiteSpace(clientId) || string.IsNullOrWhiteSpace(clientSecret)) return;
-  microsoftOptions.ClientId = clientId;
-  microsoftOptions.ClientSecret = clientSecret;
-  microsoftOptions.SaveTokens = true;
-});
+  .AddMicrosoftAccount(microsoftOptions => {
+    var clientId = builder.Configuration["Authentication:Microsoft:ClientId"];
+    var clientSecret = builder.Configuration["Authentication:Microsoft:ClientSecret"];
+    if (string.IsNullOrWhiteSpace(clientId) || string.IsNullOrWhiteSpace(clientSecret)) return;
+    microsoftOptions.ClientId = clientId;
+    microsoftOptions.ClientSecret = clientSecret;
+    microsoftOptions.SaveTokens = true;
+  });
 builder.Services.AddRazorPages();
-builder.Services.Configure<ForwardedHeadersOptions>(options =>
-{
-    options.ForwardedHeaders =
-        ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+builder.Services.Configure<ForwardedHeadersOptions>(options => {
+  options.ForwardedHeaders =
+      ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
 });
 
 var app = builder.Build();
 
-app.UseForwardedHeaders();
+app.UseForwardedHeaders(new ForwardedHeadersOptions {
+  ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+if (!app.Environment.IsDevelopment()) {
+  app.UseExceptionHandler("/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
