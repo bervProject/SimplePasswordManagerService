1ccda072f002bfc3faf1b884531609ff99e8d6d4
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2026-07-28 22:38:33 +0700

    fix: fix condition

--- SimplePasswordManagerService/Program.cs
+++ SimplePasswordManagerService/Program.cs
@@ -60,8 +60,10 @@ if (!app.Environment.IsDevelopment())
   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
   app.UseHsts();
 }
-
-app.UseHttpsRedirection();
+else
+{
+  app.UseHttpsRedirection();
+}
 
 app.UseStaticFiles();
 
